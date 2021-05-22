e2620b079388fdd9005816ce2635c693a0cd7658
HeideQueen <lucyfabz@gmail.com>
2021-05-21 23:14:29 -0500

    landing page done

---
+++ src/components/SignUpForm/SignUpForm.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+import styles from './signUpForm.module.css';
+
+function SignUpForm() {
+  return (
+    <section className={styles.formContainer}>
+      <form>
+        <h1>form placeholder</h1>
+      </form>
+    </section>
+  );
+}
+
+export default SignUpForm;
--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -1,16 +1,24 @@
 import React from 'react';
 
 import { Link } from 'react-router-dom';
+import SignUpForm from '../../components/SignUpForm/SignUpForm';
 
 import styles from './landing.module.css';
 
 function Landing() {
   return (
     <div className={styles.container}>
-      <h1>Currently Under Construction</h1>
-      <h2>
-        Go to: <Link to='/home'>Home</Link>{' '}
-      </h2>
+      <header className={styles.header}>
+        <h2>WELCOME TO A NEW WORLD OF SONOROUSNESS</h2>
+        <h1>
+          SOUND EXPERIENCE DX WILL REVOLUTIONIZE THE WAY YOU LISTEN IN MORE WAYS
+          THAN ONE
+        </h1>
+        <Link to='/home' className={styles.link}>
+          Enter The Experience
+        </Link>
+      </header>
+      <SignUpForm />
     </div>
   );
 }
